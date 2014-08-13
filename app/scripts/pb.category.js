933c70ac28e1038831c11cb0031d58341de5a63b
Amanda <arkassay@gmail.com>
2014-08-13 17:41:15 -0400

    category page scripts

---
+++ app/scripts/pb.category.js
@@ -0,0 +1,16 @@
+'use strict';
+
+pb.namespace('category');
+
+pb.category = (function() {
+
+  function init() {
+    var pagename = location.pathname;
+    var categoryContent = pb.category.content.getContent(pagename);
+    pb.category.content.setContent.(categoryContent);
+  };
+
+  return {
+    init: init
+  };
+})();
