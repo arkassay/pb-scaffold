c38f87899e01b9276052528392edab9beadd775a
Courtney Curtis <ccurtis.dezign@gmail.com>
2014-09-04 17:37:03 -0400

    edits to all content page

---
+++ app/scripts/pb.utils.js
@@ -0,0 +1,28 @@
+pb.namespace('utils');
+console.log('utils!');
+
+pb.utils = (function(){
+  function init() {
+    handlers();
+    console.log('utils init');
+    $('.additional-content').slideUp();
+  };
+
+  function handlers(){
+    $('.view-all').click(function(e){
+      e.preventDefault();
+      $('.additional-content').slideToggle();
+      $(this).children().toggleClass('hide show');
+    });
+
+  }
+
+  return {
+    init: init
+  };
+
+})();
+
+$(function() {
+  pb.utils.init();
+});
