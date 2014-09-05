465bdff3f59dafd647a95098a3ff7719c9a2eb30
Amanda <arkassay@gmail.com>
2014-09-05 14:44:58 -0400

    fixed gjslint errors

--- app/scripts/pb.utils.js
+++ app/scripts/pb.utils.js
@@ -1,15 +1,15 @@
 pb.namespace('utils');
 console.log('utils!');
 
-pb.utils = (function(){
+pb.utils = (function() {
   function init() {
     handlers();
     console.log('utils init');
     $('.additional-content').slideUp();
   };
 
-  function handlers(){
-    $('.view-all').click(function(e){
+  function handlers() {
+    $('.view-all').click(function(e) {
       e.preventDefault();
       $('.additional-content').slideToggle();
       $(this).children().toggleClass('hide show');
